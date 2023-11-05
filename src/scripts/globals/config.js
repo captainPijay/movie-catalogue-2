e531c002b7ebf7fc62484a420579c2221fd42359
captainPijay <yudharizal10@gmail.com>
2023-11-05 20:21:59 +0700

    commit ganti

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '8cc4a727cff65291a628f936499e257a',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
